9d4bcc24314a7fe01eea8e9bc5fb9bf67e5ce6e3
ayayaQ <westonjoshua88@gmail.com>
2024-09-23 03:20:14 -0700

    Client settings

---
+++ src/renderer/src/components/Settings.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+  import { onMount } from 'svelte'
+  import { saveSettings, settingsStore } from '../stores/settings'
+
+  let selectedTheme: string
+  function changeTheme(event) {
+    let theme = event.target.value
+    document.documentElement.setAttribute('data-theme', theme)
+    saveSettings({ theme })
+  }
+
+  onMount(() => {
+    selectedTheme = $settingsStore.theme
+  })
+</script>
+
+<div class="p-4">
+  <h2 class="text-2xl font-bold mb-4">Settings</h2>
+  <div class="form-control">
+    <label class="label">
+      <span class="label-text">Theme</span>
+    </label>
+    <select class="select select-bordered" value={selectedTheme} on:change={changeTheme}>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+      <option value="cupcake">Cupcake</option>
+      <option value="bumblebee">Bumblebee</option>
+      <option value="emerald">Emerald</option>
+      <option value="corporate">Corporate</option>
+      <option value="synthwave">Synthwave</option>
+      <option value="retro">Retro</option>
+      <option value="cyberpunk">Cyberpunk</option>
+      <option value="valentine">Valentine</option>
+      <option value="halloween">Halloween</option>
+      <option value="garden">Garden</option>
+      <option value="forest">Forest</option>
+      <option value="aqua">Aqua</option>
+      <option value="lofi">Lo-fi</option>
+      <option value="pastel">Pastel</option>
+      <option value="fantasy">Fantasy</option>
+      <option value="wireframe">Wireframe</option>
+      <option value="black">Black</option>
+      <option value="luxury">Luxury</option>
+      <option value="dracula">Dracula</option>
+      <option value="cmyk">CMYK</option>
+      <option value="autumn">Autumn</option>
+      <option value="business">Business</option>
+      <option value="acid">Acid</option>
+      <option value="lemonade">Lemonade</option>
+      <option value="night">Night</option>
+      <option value="coffee">Coffee</option>
+      <option value="winter">Winter</option>
+    </select>
+  </div>
+</div>
---
+++ src/renderer/src/stores/settings.ts
@@ -0,0 +1,14 @@
+import { writable } from "svelte/store";
+import type { AppSettings } from "../types/types";
+
+export const settingsStore = writable<AppSettings>({ theme: 'light' });
+
+export async function loadSettings() {
+    const settings = await (window as any).electron.ipcRenderer.invoke('get-settings');
+    settingsStore.set(settings);
+}
+
+export async function saveSettings(newSettings: AppSettings) {
+    await (window as any).electron.ipcRenderer.invoke('save-settings', newSettings);
+    settingsStore.set(newSettings);
+}
