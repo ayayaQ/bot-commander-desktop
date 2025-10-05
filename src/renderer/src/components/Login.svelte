6c475c7a29cf840c467511a13e74225f34c6edd3
ayayaQ <westonjoshua88@gmail.com>
2025-10-05 03:38:17 -0700

    Better feedback when logging in

--- src/renderer/src/components/Login.svelte
+++ src/renderer/src/components/Login.svelte
@@ -10,7 +10,7 @@
   $: username = $connectionStore.username
   $: avatar = $connectionStore.avatar
 
-  let token = '';
+  let token = ''
   function handleLogin() {
     connectionStore.ipc.connect(token)
   }
@@ -46,7 +46,9 @@
     <div class="card-body items-center text-center">
       {#if avatar}
         <div class="avatar placeholder">
-          <div class={`rounded-full ${$botStatusStore.status === 'Online' ? 'outline outline-2 outline-green-500' : $botStatusStore.status === 'Do Not Disturb' ? 'outline outline-2 outline-red-500' : $botStatusStore.status === 'Invisible' ? 'outline outline-2 outline-gray-500' : 'outline outline-2 outline-yellow-500'}`}>
+          <div
+            class={`rounded-full ${$botStatusStore.status === 'Online' ? 'outline outline-2 outline-green-500' : $botStatusStore.status === 'Do Not Disturb' ? 'outline outline-2 outline-red-500' : $botStatusStore.status === 'Invisible' ? 'outline outline-2 outline-gray-500' : 'outline outline-2 outline-yellow-500'}`}
+          >
             <img src={avatar} alt="Avatar" />
           </div>
         </div>
@@ -58,7 +60,6 @@
         </div>
       {/if}
 
-      
       {#if !$connectionStore.connected}
         {#if $settingsStore.showToken}
           <input
@@ -77,9 +78,13 @@
         {/if}
       {:else}
         <h2 class="card-title">{username}</h2>
-      <div class="text-sm"><span>{$botStatusStore.activity}{getActivityPreposition($botStatusStore)}</span><span class='font-medium'>&nbsp;{$botStatusStore.activityDetails}</span></div>
+        <div class="text-sm">
+          <span>{$botStatusStore.activity}{getActivityPreposition($botStatusStore)}</span><span
+            class="font-medium">&nbsp;{$botStatusStore.activityDetails}</span
+          >
+        </div>
         <input
-          type={$settingsStore.showToken ? "text" : "password"}
+          type={$settingsStore.showToken ? 'text' : 'password'}
           placeholder={$t('token')}
           disabled
           class="input input-bordered w-full max-w-xs"
@@ -87,14 +92,27 @@
         />
       {/if}
       {#if !$connectionStore.connected}
-        <button class="btn btn-primary w-full" on:click={handleLogin}><span class="material-symbols-outlined">login</span>{$t('login')}</button>
+        <button class="btn btn-primary w-full" on:click={handleLogin}
+          ><span class="material-symbols-outlined">login</span>{$t('login')}
+          {#if $connectionStore.isConnecting}
+            <span class="loading loading-spinner"></span>
+          {/if}
+        </button>
       {:else}
-        <button class="btn btn-primary w-full" on:click={handleLogout}><span class="material-symbols-outlined">logout</span>{$t('logout')}</button>
-        <button class="btn btn-primary w-full" on:click={generateInvite}><span class="material-symbols-outlined">mail</span>{$t('invite')}</button>
+        <button class="btn btn-primary w-full" on:click={handleLogout}
+          ><span class="material-symbols-outlined">logout</span>{$t('logout')}</button
+        >
+        <button class="btn btn-primary w-full" on:click={generateInvite}
+          ><span class="material-symbols-outlined">mail</span>{$t('invite')}</button
+        >
       {/if}
       <div class="collapse bg-base-200">
         <input type="checkbox" />
-        <div class="collapse-title text-xl font-medium px-0"><div class="flex items-center justify-center gap-2"><span class="material-symbols-outlined">settings</span><span>{$t('status')}</span></div></div>
+        <div class="collapse-title text-xl font-medium px-0">
+          <div class="flex items-center justify-center gap-2">
+            <span class="material-symbols-outlined">settings</span><span>{$t('status')}</span>
+          </div>
+        </div>
         <div class="collapse-content">
           <Status />
         </div>
--- src/renderer/src/stores/connection.ts
+++ src/renderer/src/stores/connection.ts
@@ -1,79 +1,83 @@
-import { writable } from "svelte/store";
+import { writable } from 'svelte/store'
 
 export type ConnectionState = {
-    connected: boolean;
-    username: string;
-    avatar: string;
-    token: string;
+  connected: boolean
+  isConnecting: boolean
+  username: string
+  avatar: string
+  token: string
 }
 
 function emptyState(): ConnectionState {
-    return {
-        connected: false,
-        username: "",
-        avatar: "",
-        token: "",
-    }
+  return {
+    connected: false,
+    isConnecting: false,
+    username: '',
+    avatar: '',
+    token: ''
+  }
 }
 
 function createConnectionStore() {
-    const { subscribe, set, update } = writable<ConnectionState>(emptyState());
+  const { subscribe, set, update } = writable<ConnectionState>(emptyState())
 
-    const ipc = {
-        connect,
-        disconnect,
-        generateInvite,
-        getToken
-    }
-    
-    return {
-        subscribe,
-        set,
-        update,
-        ipc
-    }
+  const ipc = {
+    connect,
+    disconnect,
+    generateInvite,
+    getToken
+  }
+
+  return {
+    subscribe,
+    set,
+    update,
+    ipc
+  }
 }
 
-export const connectionStore = createConnectionStore();
+export const connectionStore = createConnectionStore()
 
 function addConnectionListener() {
-    (window as any).electron.ipcRenderer.on('connect', (event, data) => {
-        console.log('Connected to server', data, event);
+  ;(window as any).electron.ipcRenderer.on('connect', (event, data) => {
+    console.log('Connected to server', data, event)
 
-        connectionStore.update(state => ({
-            ...state,
-            connected: true,
-            username: data.user,
-            avatar: data.avatar
-        }));
-    });
+    connectionStore.update((state) => ({
+      ...state,
+      connected: true,
+      isConnecting: false,
+      username: data.user,
+      avatar: data.avatar
+    }))
+  })
 
-    (window as any).electron.ipcRenderer.on('disconnect', (event) => {
-        console.log('Disconnected from server', event);
-    });
+  ;(window as any).electron.ipcRenderer.on('disconnect', (event) => {
+    console.log('Disconnected from server', event)
+  })
 }
 
-addConnectionListener();
+addConnectionListener()
 
 function connect(token: string) {
-    (window as any).electron.ipcRenderer.send('connect', token);
+  ;(window as any).electron.ipcRenderer.send('connect', token)
 
-    connectionStore.update(state => ({
-        ...state,
-        token: token
-    }));
+  connectionStore.update((state) => ({
+    ...state,
+    isConnecting: true,
+    token: token
+  }))
 }
 
 function disconnect() {
-    (window as any).electron.ipcRenderer.send('disconnect');
+  ;(window as any).electron.ipcRenderer.send('disconnect')
 
-    connectionStore.set(emptyState());
+  connectionStore.set(emptyState())
 }
 
 async function generateInvite() {
-    return await (window as any).electron.ipcRenderer.invoke('generate-invite');
+  return await (window as any).electron.ipcRenderer.invoke('generate-invite')
 }
 
 async function getToken() {
-    return await (window as any).electron.ipcRenderer.invoke('get-token');
+  return await (window as any).electron.ipcRenderer.invoke('get-token')
 }
